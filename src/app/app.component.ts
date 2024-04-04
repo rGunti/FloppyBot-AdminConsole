8ea440152301f0f0db09b4c7e0e0801875620d6d
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-04-04 23:30:41 +0200

    more add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapFloppy2 } from '@ng-icons/bootstrap-icons';
+
 @Component({
   selector: 'fac-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, NgIconComponent],
+  providers: [
+    provideIcons({
+      bootstrapFloppy2,
+    }),
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
