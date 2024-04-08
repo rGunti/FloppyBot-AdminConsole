a9050b85f2d38f296a353397eab0d10e9ba2b45f
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-04-08 19:43:14 +0200

    profile skeleton

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapPersonCircle, bootstrapShieldCheck, bootstrapKey, bootstrapBox } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'fac-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    NgIconComponent,
+  ],
+  providers: [
+    provideIcons({
+      bootstrapPersonCircle,
+      bootstrapShieldCheck,
+      bootstrapKey,
+      bootstrapBox,
+    }),
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  private readonly formBuilder = inject(FormBuilder);
+  readonly form = this.formBuilder.group({
+    username: [''],
+    userId: [''],
+    email: [''],
+    updated: [new Date()],
+  });
+}
