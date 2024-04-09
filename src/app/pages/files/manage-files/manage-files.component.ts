24bd4466f89e13e411a19c541f9132724cc542bc
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-04-09 22:12:04 +0200

    file table adjustments

--- src/app/pages/files/manage-files/manage-files.component.ts
+++ src/app/pages/files/manage-files/manage-files.component.ts
@@ -5,17 +5,39 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { ChannelSelectorComponent } from '../../../components/channel-selector/channel-selector.component';
 import { Channel } from '../../../api/entities';
+import { MatIconModule } from '@angular/material/icon';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { MatButtonModule } from '@angular/material/button';
+import { bootstrapDownload, bootstrapFileEarmark, bootstrapTrash } from '@ng-icons/bootstrap-icons';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'fac-manage-files',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule, ChannelSelectorComponent],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    ChannelSelectorComponent,
+    MatIconModule,
+    NgIconComponent,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
+  providers: [
+    provideIcons({
+      bootstrapTrash,
+      bootstrapFileEarmark,
+      bootstrapDownload,
+    }),
+  ],
   templateUrl: './manage-files.component.html',
   styleUrl: './manage-files.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ManageFilesComponent implements AfterViewInit {
-  readonly displayedColumns: string[] = ['name', 'size', 'date'];
+  readonly displayedColumns: string[] = ['icon', 'name', 'size', 'actions'];
   readonly dataSource = new MatTableDataSource([
     { name: 'File 1', size: '1.2 MB', date: '2021-01-01' },
     { name: 'File 2', size: '2.5 MB', date: '2021-01-02' },
@@ -38,5 +60,15 @@ export class ManageFilesComponent implements AfterViewInit {
 
   onChannelSelected(selectedChannel: Channel | null | undefined) {
     console.log('Selected channel:', selectedChannel);
+  }
+
+  getDownloadTooltip(file: unknown) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return `Download file "${(file as any).name}"`;
+  }
+
+  getDeleteTooltip(file: unknown) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return `Delete file "${(file as any).name}"`;
   }
 }
