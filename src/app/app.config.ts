f96c7ab98105d9ae1afa09086b10919bd6e6582a
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-04-09 19:27:53 +0200

    added pwa support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { TitleStrategy, provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideNgIconsConfig, withContentSecurityPolicy } from '@ng-icons/core';
 import { FloppyBotTitleStrategy } from './utils/title-strategy';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +16,9 @@ export const appConfig: ApplicationConfig = {
       provide: TitleStrategy,
       useClass: FloppyBotTitleStrategy,
     },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
