a06b5fc07ce8fbbdfab9a8b1b12e24f28e8fb347
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-04-10 23:42:12 +0200

    better 404

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { bootstrapQuestionDiamond } from '@ng-icons/bootstrap-icons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'fac-not-found',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, NgIconComponent],
+  providers: [
+    provideIcons({
+      bootstrapQuestionDiamond,
+    }),
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
