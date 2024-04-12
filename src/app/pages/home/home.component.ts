3c67a61f073861b0c8e9a323806f3f0a581533d8
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-04-12 19:20:44 +0200

    home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { bootstrapBook, bootstrapEyeglasses, bootstrapFloppy2, bootstrapGithub } from '@ng-icons/bootstrap-icons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'fac-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon, NgIconComponent, MatButtonModule],
+  providers: [
+    provideIcons({
+      bootstrapFloppy2,
+      bootstrapGithub,
+      bootstrapEyeglasses,
+      bootstrapBook,
+    }),
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
