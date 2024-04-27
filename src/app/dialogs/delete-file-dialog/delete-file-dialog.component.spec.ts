318aa3e29d7304f627159f248eccddd3b42b6442
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-04-27 21:53:26 +0200

    fixing dialogs

--- src/app/dialogs/delete-file-dialog/delete-file-dialog.component.spec.ts
+++ src/app/dialogs/delete-file-dialog/delete-file-dialog.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+
+import { FileHeader } from '../../api/entities';
+import { FileSizePipe } from '../../utils/files/file-size.pipe';
 
 import { DeleteFileDialogComponent } from './delete-file-dialog.component';
 
@@ -8,10 +13,20 @@ describe('DeleteFileDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DeleteFileDialogComponent]
-    })
-    .compileComponents();
-    
+      imports: [DeleteFileDialogComponent, MatDialogModule, MatButtonModule, FileSizePipe],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            id: 'file-id',
+            fileName: 'file-name',
+            fileSize: 100,
+            mimeType: 'image/png',
+          } as FileHeader,
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DeleteFileDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
