ceb06f8705f55dfd920e04efdde2a30b0d888aff
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-04-30 23:44:53 +0200

    style fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { GlobalLoadingIndicatorComponent } from './components/global-loading-indicator/global-loading-indicator.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
 @Component({
   selector: 'fac-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent, GlobalLoadingIndicatorComponent],
+  imports: [RouterOutlet, NavigationComponent],
   providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
