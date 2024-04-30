c6e85ec7fcbf38d87dff5b11aff33bf59022afe3
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-05-01 01:25:25 +0200

    fixed unit test

--- src/app/pages/settings/channel-aliases/channel-aliases.component.spec.ts
+++ src/app/pages/settings/channel-aliases/channel-aliases.component.spec.ts
@@ -1,3 +1,5 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChannelAliasesComponent } from './channel-aliases.component';
@@ -8,10 +10,10 @@ describe('ChannelAliasesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ChannelAliasesComponent]
-    })
-    .compileComponents();
-    
+      imports: [ChannelAliasesComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ChannelAliasesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
