ff7fdb95b07ba9c3f1f174d7df26ec820b5eeb72
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-05-01 16:07:18 +0200

    added script to version app

---
+++ version.js
@@ -0,0 +1,25 @@
+const { gitDescribeSync } = require('git-describe');
+const { version } = require('./package.json');
+const { resolve, relative } = require('path');
+const { writeFileSync } = require('fs');
+
+const gitInfo = gitDescribeSync({
+  dirtyMark: false,
+  dirtySemver: false
+});
+
+gitInfo.version = version;
+
+const srcDir = resolve(__dirname, 'src');
+const jsonAsset = resolve(srcDir, 'assets', 'version.json');
+console.log('Writing JSON version info to', jsonAsset);
+writeFileSync(jsonAsset, JSON.stringify(gitInfo, null, 4), { encoding: 'utf-8' });
+
+const tsFile = resolve(srcDir, 'version', 'version.ts');
+console.log('Writing TS version info to', tsFile);
+writeFileSync(tsFile,
+`// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = ${JSON.stringify(gitInfo, null, 4)};
+/* tslint:enable */
+`, { encoding: 'utf-8' });
