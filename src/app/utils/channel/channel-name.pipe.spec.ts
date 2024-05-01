3ecaa51aeccd6b5eb0b43c71e77b433f9d182f1a
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-05-01 13:57:32 +0200

    channel pipe testing

--- src/app/utils/channel/channel-name.pipe.spec.ts
+++ src/app/utils/channel/channel-name.pipe.spec.ts
@@ -2,6 +2,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { Channel } from '../../api/entities';
 import { UserApiService } from '../../api/user-api.service';
 
 import { ChannelService } from './channel.service';
@@ -20,4 +21,18 @@ describe('ChannelNamePipe', () => {
   it('create an instance', () => {
     expect(pipe).toBeTruthy();
   });
+
+  const cases: [Channel | null | undefined, string | undefined][] = [
+    [{ interface: 'Twitch', channel: 'NotARealChannel' }, 'Twitch/NotARealChannel'],
+    [{ interface: 'Twitch', channel: 'NotARealChannel', alias: 'Not A Real Channel' }, 'Not A Real Channel'],
+    [null, undefined],
+    [undefined, undefined],
+  ];
+
+  for (const caseData of cases) {
+    it(`formats channel ${JSON.stringify(caseData[0])} to "${caseData[1]}"`, () => {
+      const result = pipe.transform(caseData[0]);
+      expect(result).toBe(caseData[1]);
+    });
+  }
 });
