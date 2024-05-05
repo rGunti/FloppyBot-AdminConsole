b0d76f3f549cf306472588c667ff2f6b5ed2afb2
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-05-05 02:02:47 +0200

    fixed tests

--- src/app/dialogs/built-in-command-editor-dialog/built-in-command-editor-dialog.component.spec.ts
+++ src/app/dialogs/built-in-command-editor-dialog/built-in-command-editor-dialog.component.spec.ts
@@ -1,4 +1,8 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { BuiltInCommandEditorDialogComponent } from './built-in-command-editor-dialog.component';
 
@@ -8,10 +12,35 @@ describe('BuiltInCommandEditorDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BuiltInCommandEditorDialogComponent]
-    })
-    .compileComponents();
-    
+      imports: [BuiltInCommandEditorDialogComponent, NoopAnimationsModule],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            channelId: 'Twitch/AFakeChannel',
+            report: {
+              command: {},
+              configuration: {
+                id: 'Twitch/AFakeChannel/!fakeCommand',
+                channelId: 'Twitch/AFakeChannel',
+                commandName: '!fakeCommand',
+                disabled: false,
+                customCooldown: false,
+                customCooldownConfiguration: [],
+                privilegeLevel: 'Moderator',
+              },
+            },
+          },
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(BuiltInCommandEditorDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
