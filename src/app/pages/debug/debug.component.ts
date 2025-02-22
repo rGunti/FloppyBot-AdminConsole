f3c469d7a903f1ddbcfe42133d6bd5c2dceeafb5
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2025-02-22 18:30:40 +0100

    fixed slide toggle theme

--- src/app/pages/debug/debug.component.ts
+++ src/app/pages/debug/debug.component.ts
@@ -3,13 +3,14 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDivider } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 import { ChannelService } from '../../utils/channel/channel.service';
 
 @Component({
   selector: 'fac-debug',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatDivider, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatDivider, MatIconModule, MatSlideToggle],
   templateUrl: './debug.component.html',
   styleUrl: './debug.component.scss',
 })
